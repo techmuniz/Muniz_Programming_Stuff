f15bb966de77307e9589b09e715eeba89b8c0719
Paulo Muniz <paulomunizjw@gmail.com>
2022-09-02 18:21:29 -0300

    ok

--- 100_days_BootCamp/project_10 - Treasure_map.py
+++ 100_days_BootCamp/project_10 - Treasure_map.py
@@ -7,14 +7,10 @@ print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 # 🚨 Don't change the code above 👆
 
-#Write your code below this row 👇
 
 horizontal = int(position[0])
 vertical = int(position[1])
 
 map[vertical - 1][horizontal - 1] = "X"
 
-#Write your code above this row 👆
-
-# 🚨 Don't change the code below 👇
 print(f"{row1}\n{row2}\n{row3}")
