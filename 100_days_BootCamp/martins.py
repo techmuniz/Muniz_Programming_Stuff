b28c24089b84a45c89ce0cd55172b8a30e03b402
Paulo Muniz <paulomunizjw@gmail.com>
2022-09-03 03:11:06 -0300

    ok

--- 100_days_BootCamp/martins.py
+++ 100_days_BootCamp/martins.py
@@ -1,4 +1,5 @@
-def helloworld (name):
+#Mess around with helloworld
+'''def helloworld (name):
     while True:
         name = input("Say my name: ")
         if name.upper() != "HELLO WORLD":
@@ -6,7 +7,22 @@ def helloworld (name):
         else:
             break
 
-password = helloworld("")
+password = helloworld("")'''
 
+#Counters & Sum
+'''
+sum = 0
+counter = 0
+
+for x in range (1, 976):
+    counter = counter + 1
+
+for x in range (1, 976):
+    sum = sum + x
+
+
+print (counter, sum)
+
+'''
 
 
